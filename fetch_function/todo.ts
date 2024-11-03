0e3c3c2e095e3ed1aa4108e90a961c99d69045c9
横田昴輝 <kokiyokotakoki@gmail.com>
2024-11-03 11:49:29 +0900

    ok

--- fetch_function/todo.ts
+++ fetch_function/todo.ts
@@ -1,12 +1,5 @@
-import { headers } from 'next/headers'
+import { getDomain } from "@/lib/serverCommon";
 
-const getDomain = (): string => {
-    const headersData = headers();
-    const host = headersData.get('host');
-    const protocol = headersData.get('x-forwarded-proto') ?? host?.startsWith('localhost') ? 'http' : 'https';
-    const apiBase = `${protocol}://${host}`;
-    return apiBase;
-}
 export async function getTodo(id: number) {
     const res = await fetch(
         `${getDomain()}/api/todos?id=${id}`,

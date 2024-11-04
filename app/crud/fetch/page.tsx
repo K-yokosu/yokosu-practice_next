b5f4f6c330cd254f75d5786e10f523929f0ecbd3
横田昴輝 <kokiyokotakoki@gmail.com>
2024-11-05 01:58:37 +0900

    11

--- app/crud/fetch/page.tsx
+++ app/crud/fetch/page.tsx
@@ -1,9 +1,11 @@
 import TodoList from "./todoList";
-import { getTodos } from "@/request/todo";
+// import { getTodos } from "@/request/todo";
+import prisma from '@/lib/prisma'
 import { TodoType } from "@/types/todo";
 
 export default async function Page() {
-    const todos: TodoType[] = await getTodos();
+    // const todos: TodoType[] = await getTodos();
+    const todos = await prisma.todos.findMany();
     return (
         <TodoList todos={todos} />
     );

12904ef9c71a80ed05212fa3807a51e866c51b15
横田昴輝 <kokiyokotakoki@gmail.com>
2024-11-05 00:00:21 +0900

    3

--- fetch_function/todo.ts
+++ fetch_function/todo.ts
@@ -1,29 +1,29 @@
-import { getDomain } from "@/lib/serverCommon";
-import { TodoType } from "@/types/todo";
+// import { getDomain } from "@/lib/serverCommon";
+// import { TodoType } from "@/types/todo";
 
-// ここ今使うことないけど練習pjやし一応置いとこか
-export async function getTodo(id: number): Promise<TodoType | null> {
-    const res = await fetch(
-        `${getDomain()}/api/todos?id=${id}`,
-        {
-            method: 'GET', 
-            // cache: "no-store"
-            next: {revalidate: 30},
-        }
-    ).then(res => res.json());
-    const todo: TodoType | null = res.todo ? {...res.todo} : null;
-    return todo;
-}
+// // ここ今使うことないけど練習pjやし一応置いとこか
+// export async function getTodo(id: number): Promise<TodoType | null> {
+//     const res = await fetch(
+//         `${getDomain()}/api/todos?id=${id}`,
+//         {
+//             method: 'GET', 
+//             // cache: "no-store"
+//             next: {revalidate: 30},
+//         }
+//     ).then(res => res.json());
+//     const todo: TodoType | null = res.todo ? {...res.todo} : null;
+//     return todo;
+// }
 
-export async function getTodos(): Promise<TodoType[]> {
-    const res = await fetch(
-        `${getDomain()}/api/todos`, 
-        {
-            method: 'GET', 
-            // cache: "no-store"
-            next: {revalidate: 30}
-        }
-    ).then(res => res.json());
-    const todos: TodoType[] = res.todos;
-    return todos;
-}
+// export async function getTodos(): Promise<TodoType[]> {
+//     const res = await fetch(
+//         `${getDomain()}/api/todos`, 
+//         {
+//             method: 'GET', 
+//             // cache: "no-store"
+//             next: {revalidate: 30}
+//         }
+//     ).then(res => res.json());
+//     const todos: TodoType[] = res.todos;
+//     return todos;
+// }

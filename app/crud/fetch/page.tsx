6daf708690f52035e7e84d305b7b957b7935fea6
横田昴輝 <kokiyokotakoki@gmail.com>
2024-11-05 00:26:12 +0900

    8

--- app/crud/fetch/page.tsx
+++ app/crud/fetch/page.tsx
@@ -1,6 +1,7 @@
 import TodoList from "./todoList";
 import { getTodos } from "@/request/todo";
 import { TodoType } from "@/types/todo";
+
 export default async function Page() {
     const todos: TodoType[] = await getTodos();
     return (
